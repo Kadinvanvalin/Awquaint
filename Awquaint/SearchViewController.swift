cc259a617802774a2baad10940eea023e5b9a469
Kadin & Katherine Thongsakounh <pair+kadinvanvalin.kthong3@devbootcamp.com>
2017-10-21 21:55:48 -0500

    add table with prototype styles

--- Awquaint/SearchViewController.swift
+++ Awquaint/SearchViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let list = ["javascript", "pb & j", "la croix"]
+    
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        print(list.count)
+        return(list.count)
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+//        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = list[indexPath.row]
+        print(list[indexPath.row])
+        return(cell)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
