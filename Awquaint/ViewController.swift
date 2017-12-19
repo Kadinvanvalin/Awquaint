2210f504159ee9f7ab8567cddc90f35310ee20ed
kadinvanvalin <kadinvanvalin@yahoo.com>
2017-12-19 13:37:51 -0500

    Config for personal project

--- Awquaint/ViewController.swift
+++ Awquaint/ViewController.swift
@@ -13,37 +13,37 @@ import SwiftyJSON
 class ViewController: UIViewController, UITextFieldDelegate {
 
     //outlets
-    
+
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+
         self.email.delegate = self
         self.password.delegate = self
     }
-    
-    
+
+
     // hide keyboard when users touches outside keyboard
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    
+
     @IBAction func menuButton(_ sender: UIBarButtonItem) {
     //show menu?
     }
-    
+
     // when user presses return key, keyboard hides
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
+
         email.resignFirstResponder()
         password.resignFirstResponder()
-        
+
         return true
     }
-    
+
     //actions
     @IBAction func loginButton(_ sender: Any) {
         login()
@@ -51,7 +51,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func signUpButton(_ sender: Any) {
         signUp()
     }
-    
+
 //    override func didReceiveMemoryWarning() {
 //        super.didReceiveMemoryWarning()
 //        // Dispose of any resources that can be recreated.
@@ -62,26 +62,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 "email": email.text,
                 "password": password.text
         ]
-        
-        Alamofire.request("https://awquaint-server.herokuapp.com/sessions", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+
+        Alamofire.request("https://awquaint.herokuapp.com/sessions", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
             print("Request: \(String(describing: response.request))")   // original url request
             print("Response: \(String(describing: response.response))") // http url response
             print("Result: \(response.result)")                         // response serialization result
-            
+
             if response.response?.statusCode == 200 {
                 if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "profileViewController") as? ProfileViewController {
-                 
+
                          let json = JSON(response.result.value)
-                    
+
                         viewController.namePassed = json["name"].stringValue
                         viewController.idPassed = json["id"].stringValue
-                    
+
                        print(json["name"])
                        print(json["id"])
-                    
-                        
+
+
                     self.present(viewController, animated: true, completion: nil)
-                } 
+                }
             } else {
                 // alert user
                 let alert = UIAlertController(title: "Incorrect password", message: "Incorrect password", preferredStyle: .alert)
@@ -90,7 +90,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
-    
+
     func signUp(){
         if let signUpViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "signUpViewController") as? SignUpViewController {
             self.present(signUpViewController, animated: true, completion: nil)
